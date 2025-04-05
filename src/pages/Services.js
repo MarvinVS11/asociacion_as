7b89a959874c5bd72b8064fa2ccfece261f98e5d
MarvinVS11 <marvi11@hotmail.es>
2025-04-04 21:28:01 -0600

    comentario

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container/*, Row, Col, Card */} from "react-bootstrap";
 import "../scss/_transitions.scss";
 import '../javascript/events'
 import '../javascript/collapse'
